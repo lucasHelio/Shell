749a68f9c51b2aef3a98cdf6d423cc45a8d8833a
lucasHelio <lucasheliosr@hotmail.com>
2021-06-06 13:03:43 -0300

    bugs do cd resolvidos e filhos terminados

--- shell.c
+++ shell.c
@@ -13,6 +13,10 @@
 #define FOREGROUND_EXECUTION 1
 
 #define BUFFERLIMITE 200
+
+const char *STATUS_STRING[] = { "Executando", "Suspenso", "Terminado"};
+const char *COMANDOBUILDIN[] = { "cd", "jobs", "fg", "bg", "exit", "clear"};
+
 static jmp_buf env;
 int jump_active = 0;
 pid_t pid;
@@ -24,23 +28,19 @@ typedef struct jobs
     int modo;
     char  status[20];
     pid_t pid_process;
-
+    int cont;
 }jobs;
 
-int jobsexistentes=1;
+int jobsexistentes=0;
 jobs* jobscriados;
 
-char *getcwd(char *buf, size_t size);
-
-
-
-const char *STATUS_STRING[] = { "Executando", "Suspenso", "Terminado"};
+//char *getcwd(char *buf, size_t size);
 
 void adicionaJobs(jobs job, pid_t pid_process, int modo, char* nome)
 {
     
     
-    job.id = jobsexistentes;
+    job.id = jobsexistentes+1;
 
     strcpy(job.nome, nome);
 
@@ -50,7 +50,9 @@ void adicionaJobs(jobs job, pid_t pid_process, int modo, char* nome)
     
     job.modo = modo;
 
-    jobscriados[jobsexistentes-1] = job;
+    job.cont =0;
+
+    jobscriados[jobsexistentes] = job;
 
     jobsexistentes++;
     return;
@@ -58,7 +60,7 @@ void adicionaJobs(jobs job, pid_t pid_process, int modo, char* nome)
 
 void suspendeJobs(pid_t pid_process)
 {
-    for(int i =0;i<jobsexistentes;i++)
+    for(int i =0;i<=jobsexistentes;i++)
     {
         if(jobscriados[i].pid_process == pid_process)
         {
@@ -70,9 +72,23 @@ void suspendeJobs(pid_t pid_process)
     return;
 }
 
+void terminaJobs(pid_t pid_process)
+{
+    for(int i=0; i<=jobsexistentes; i++)
+    {
+        if(jobscriados[i].pid_process == pid_process)
+        {
+            strcpy(jobscriados[i].status, STATUS_STRING[2]);//seta como suspenso
+            //kill(pid_process, SIGSTOP); //Faz o processo parar
+            break;
+        }
+    }
+    return;
+}
+
 void eliminaJobs(pid_t pid_process) //seta o status do job como terminado
 {
-    for(int i =0; i< jobsexistentes;i++)
+    for(int i =0; i<= jobsexistentes;i++)
     {
         if(jobscriados[i].pid_process == pid_process){
             strcpy(jobscriados[i].status, STATUS_STRING[2]);// seta como terminado
@@ -87,9 +103,11 @@ void listjobs()//printa os jobs que ainda não foram terminados
 { 
     for(int i=0;i<jobsexistentes;i++)
     {
-        if(strcmp(jobscriados[i].status, STATUS_STRING[2])!=0)
+        if(jobscriados[i].cont == 0 && jobscriados[i].id !=0)
         {
-            printf("id: [%d] nome: %s \tstatus: %s pid process: %d\n", jobscriados[i].id, jobscriados[i].nome, jobscriados[i].status, jobscriados[i].pid_process);
+            printf("[%d] \t%s \t%s \tpid:%d\n", jobscriados[i].id, jobscriados[i].status, jobscriados[i].nome, jobscriados[i].pid_process);
+            if(strcmp(jobscriados[i].status, STATUS_STRING[2])==0)//certifica de printar uma unica vez o terminado dps q ele acaba
+                jobscriados[i].cont++;
         }
     }
     return;
@@ -134,12 +152,10 @@ void leInput(char *input)
     return;
 }
 
-
 void limpaTerminal()
 {
     printf("\033[H\033[J");//limpa o terminal
 }
-
 
 void parsedInput(char *comando, char ** destino, char separador) //divide os comandos dados na entrada e armazena no vetor destino
 {
@@ -161,7 +177,6 @@ void parsedInput(char *comando, char ** destino, char separador) //divide os com
     return;
 }
 
-
 char * ultimoElemCaminho(char ** caminho)
 {
     for (int i=0; i< BUFFERLIMITE;i++)
@@ -171,17 +186,20 @@ char * ultimoElemCaminho(char ** caminho)
     }
 }
 
-
 void cd(char *caminho)
 {
-    if(chdir(caminho) != 0) 
+    if(caminho == NULL)
     {
-        perror("erro ao tentar mudar de diretorio\n");
+        printf("argumento esperado para cd\n");
         return;
     }
-    else 
+    else
     {
-        chdir(caminho); 
+        if(chdir(caminho) != 0) 
+        {
+            perror("erro ao tentar mudar de diretorio\n");
+            return;
+        }
     }
     return;
 }
@@ -211,14 +229,9 @@ void printaCaminho()
 
 void exec_nao_buildin(char **comando)//, char *tipo, char *diretorio)
 {
-    //printf("comando[0]: %s\n", comando[0]);
-    //printf("comando[1]: %s\n", comando[1]);
-    //printf("comando[0]: %s", comando[0]);
-    if (execvp(comando[0], comando) == -1) {
-    perror("Error");
-    }
+    if (execvp(comando[0], comando) == -1)
+        perror("Error");
 }
-
 
 void exec_buildin(char **comando)//identifica o comando e redireciona para o proprio;
 {
@@ -258,29 +271,19 @@ void exec_buildin(char **comando)//identifica o comando e redireciona para o pro
 
 }
 
-
-
-
 int ehBuildin(char *comando) //valida se o comando eh buildin ou nao
 {
-    char *comandobuildin[] = { "cd", "jobs", "fg", "bg", "exit", "clear"};
-    //comandobuildin[0]="cd";
-    //comandobuildin[1]="jobs";
-    //comandobuildin[2]="fg";
-    //comandobuildin[3]="bg";
-    //comandobuildin[4]="exit";
-    //comandobuildin[5]="clear";
+    //char *comandobuildin[] = { "cd", "jobs", "fg", "bg", "exit", "clear"};
+    
     for(int i=0 ; i< 6;i++)
     {
-        if(strcmp(comando, comandobuildin[i])==0)
+        if(strcmp(comando, COMANDOBUILDIN[i])==0)
         {
             return 1; // achou comando build in
         }
     }
     return 0; // nao achou um comando build in
 }
-
-
 
 void sigint_handler(int sig) {
     //puts("entrou sighand");
@@ -304,6 +307,31 @@ void sigint_handler(int sig) {
         siglongjmp(env, 42);
         return;
     }
+    if (sig == SIGCHLD){ //essa merda n funciona pfvr consertar obg
+        /*
+        //char *jobsdonebuffer;
+        pid_t pid;
+        int status;// donebuffersize;
+        //struct job *jobfilho = NULL;
+        pid = waitpid(WAIT_ANY, &status, WUNTRACED|WNOHANG);
+        printf("pid: %d\n", pid);
+        if(pid>0)
+        {
+            if(WIFEXITED(status)){
+                eliminaJobs(pid);
+            }
+            else if(WIFSIGNALED(status))
+                terminaJobs(pid);
+            else if(WIFSTOPPED(status))
+                printf("to aqui");
+            return;  
+        }
+        //terminaJobs(pid); //caso só precise mudar o status
+        //eliminaJobs(pidt); //caso o processo fique como zumbi 
+        */
+        eliminaJobs(pid);
+
+    }
 
 }
 
@@ -314,31 +342,33 @@ int main()
     //pid_t pid;
     int modo = FOREGROUND_EXECUTION;
     int status;
-    
-
 
     __fpurge(stdin);
     limpaTerminal();
+
+    char *comando= (char *) malloc(BUFFERLIMITE*sizeof(char));    
     char **caminho = (char **) malloc(BUFFERLIMITE*sizeof(char *));
-    for(int i=0; i<BUFFERLIMITE; i++)
+    char **comandoSeparado =(char **) malloc(BUFFERLIMITE*sizeof(char *));
+    for(int i=0; i<BUFFERLIMITE; i++) //alocaçao caminho
     {
         caminho[i]= (char*) malloc(BUFFERLIMITE*sizeof(char));
     }
-    if(caminho == NULL)return -1;
-    
-    char *comando= (char *) malloc(BUFFERLIMITE*sizeof(char));    
-    char **comandoSeparado =(char **) malloc(BUFFERLIMITE*sizeof(char *));
-    for(int i=0; i<BUFFERLIMITE; i++)
+    for(int i=0; i<BUFFERLIMITE; i++) //alocaçao comandoSeparado
     {
         comandoSeparado[i]= (char*) malloc(BUFFERLIMITE*sizeof(char));
     }
+    
+    if(caminho == NULL)return -1;
+    if(comandoSeparado == NULL)return -1;
+
 
     caminho[0][0]= '~';
     caminho[1]=NULL;
+    //setpgid();
     
     signal(SIGINT, sigint_handler);//control c tratado na main
     signal(SIGTSTP, sigint_handler);//control z tratado na main
-
+    signal(SIGCHLD, sigint_handler);// trata o termino de um filho
     while(1) //enquanto a concha existe ele está rodando
     {
         if (sigsetjmp(env, 1) == 42)
@@ -351,7 +381,8 @@ int main()
         
         leInput(comando);
         
-        if (comando[strlen(comando) - 1] == '&') {
+        if (comando[strlen(comando) - 1] == '&') 
+        {
             modo = BACKGROUND_EXECUTION;
             comando[strlen(comando) - 1] = '\0';
         }
@@ -362,11 +393,9 @@ int main()
         // se nao for cria filho
         if(!ehBuildin(comandoSeparado[0])){
             pid = fork();
-            //if(fork()== 0){   //criando filho
-            if(pid == 0){   //criando filho
-                //printf("valor do if: %d\n", sigsetjmp(env, 1));
-                
-                //jump_active = 1;
+            
+            if(pid == 0) //criando filho
+            {   
                 exec_nao_buildin(comandoSeparado);//posso fechar?
             }
             else //processo pai esperando filho retornar
@@ -376,12 +405,15 @@ int main()
                 {
                     waitpid(pid, &status, 0);
                 }
-                //else{
-                //
-                //}    
+                else{ //printa o id e o pid do processo filho
+                    //listjobs();
+                    //printf("jobs existentes: %d\n", jobsexistentes);
+                    printf("[%d] \t%d\n", jobscriados[jobsexistentes-1].id, jobscriados[jobsexistentes-1].pid_process);
+                    //printf("job :[%d] \t%d\n", jobscriados[jobsexistentes].id, jobscriados[jobsexistentes].pid_process);
+                    //printf("job :[%d] \t%d\n", jobscriados[jobsexistentes+1].id, jobscriados[jobsexistentes+1].pid_process);
+                }
             }
         }
-
         else{
             exec_buildin(comandoSeparado);
         }
